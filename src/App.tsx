678158284316641ea6711eae2630937f51893ece
Matt Ruder <matt.j.ruder@gmail.com>
2023-05-30 18:51:59 -0500

    edit proxy and fetch call

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from "react";
 
 function App() {
+
+ const [appData, setAppData] = useState(null);
+ const [loading, setLoading] = useState(true);
+ const [error, setError] = useState(null);
+
+
+
+ useEffect(() => {
+  fetch(`/api/people`)
+   .then(response => response.json())
+   .then(data => console.log(data))
+   .catch(error => console.log(error))
+   
+ 
+ }, []);
+
+
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Rudero Vida</h1>
     </div>
   );
 }
